5a2580365b6aa0e73310986084ab0c5260e73b61
DaffaXD <79232562+DaffaXD@users.noreply.github.com>
2022-01-25 08:55:57 +0700

    Update untitled.js

--- assets/js/untitled.js
+++ assets/js/untitled.js
@@ -34,7 +34,7 @@ $(function() {
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html,body').animate({
-          scrollTop: target.offset().top + 10
+          scrollTop: target.offset().top
         }, 1000); // The number here represents the speed of the scroll in milliseconds
         return false;
       }
