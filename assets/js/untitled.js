08a388dbdda750a0168ef6ed66440247fffb84db
DaffaXD <79232562+DaffaXD@users.noreply.github.com>
2022-01-25 19:46:15 +0700

    Update untitled.js

--- assets/js/untitled.js
+++ assets/js/untitled.js
@@ -59,13 +59,12 @@ const items = document.querySelectorAll(".accordion button");
 
 function toggleAccordion() {
   const itemToggle = this.getAttribute('aria-expanded');
-  
-  for (i = 0; i < items.length; i++) {
-    items[i].setAttribute('aria-expanded', 'false');
-  }
-  
+
   if (itemToggle == 'false') {
     this.setAttribute('aria-expanded', 'true');
+  }
+  else if (itemToggle == 'true') {
+    this.setAttribute('aria-expanded', 'false');
   }
 }
 
